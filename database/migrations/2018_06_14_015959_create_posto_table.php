cf8d100aaf630c7e66271953da6da74cf0512486
Rafael Greca <rafaelgreca@unifei.edu.br>
2018-06-13 23:19:54 -0300

    Crudbooster

---
+++ database/migrations/2018_06_14_015959_create_posto_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posto', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('nome',200);
+			$table->string('telefoneContato', 30);
+			$table->string('cidade',100);
+			$table->string('estado');
+			$table->string('bairro',80);
+			$table->string('rua',300);
+			$table->string('numero',20);
+			$table->string('complemento',400)->nullable($value = true);
+			$table->string('24horas');
+			$table->string('horarioabertura');
+			$table->string('horariofechamento');
+			$table->string('cartao');
+			
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posto');
+    }
+}
